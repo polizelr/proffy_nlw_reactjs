23432e5e6616fc740bbae0ce83b6a817534ff095
Rafaela Polizel <rafaela.polizel@pca.com.br>
2020-08-04 23:13:31 -0300

    Added assets and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './assets/styles/global.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+
+      <button type="button"> Clique </button>
     </div>
   );
 }
